11dd3d02cc145b7921be390d5f5cfe0c541c441a
causeys <61594780+causeys@users.noreply.github.com>
2023-07-25 21:04:38 -0400

    Create About.jsx

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,16 @@
+// ABOUT PAGE
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className = "about">
+            <h1 className = "title-about">About</h1>
+            <div className = "body-about">
+                <p>My name is Sophia C. and I created this web application in order to learn some new things and combine two of my biggest passions: coding and fiber arts. I am in the process of drafting knit and crochet patterns that will be available for purchase and this web app will be used in conjunction with those patterns.</p>
+                <p>I earned my B.S. in Human Development at Cornell University and then decided to pursue coding. I recently earned my master's in computer science at Kent State University. When I'm not coding or doing yarnie things I am either baking, reading, or watching French reality tv shows.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
